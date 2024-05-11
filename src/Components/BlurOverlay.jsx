c0067c35c0ddfecf6299450b53fe3cb14d3aadea
saumyfsdcd <samcode46@gmail.com>
2024-05-11 09:46:12 +0530

    sdf

---
+++ src/Components/BlurOverlay.jsx
@@ -0,0 +1,8 @@
+const BlurOverlay = ({ isOpen }) => (
+  <div
+    className={`fixed inset-0  bg-gray-800 bg-opacity-50 transition duration-200 ease-in-out transform ${isOpen ? 'opacity-100' : 'opacity-0'
+      }`}
+  />
+);
+
+export default BlurOverlay;
