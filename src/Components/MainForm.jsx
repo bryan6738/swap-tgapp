cfde753919823c4a7649df7b357f6d8127356ffb
codeworld7512 <codeworld7512@hotmail.com>
2024-05-15 16:46:08 +0200

    fix coin list

--- src/Components/MainForm.jsx
+++ src/Components/MainForm.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
-import logo from '../assets/home/Exchanger/btc.svg'
-import etc from '../assets/home/Exchanger/eth.svg'
 import load from '../assets/home/Exchanger/loadarrow.svg'
 import { IoIosArrowDown } from "react-icons/io";
 import { FaUnlockAlt } from "react-icons/fa";
 import axios from 'axios';
+
+const popularCoins = ['btc', 'eth', 'usdt', 'bnb', 'sol', 'usdc', 'xrp', 'ton', 'doge', 'ada',
+  'shib', 'avax', 'trx', 'dot', 'bch', 'near', 'link', 'matic', 'ltc', 'icp']
 
 const MainForm = (props) => {
   const { exchangeInfo, setExchangeInfo } = props;
@@ -15,7 +16,6 @@ const MainForm = (props) => {
   const [toCoin, setToCoin] = useState(exchangeInfo.toCoin);
   const [fromCoinAmount, setFromCoinAmount] = useState(exchangeInfo.fromCoinAmount);
   const [toCoinAmount, setToCoinAmount] = useState(exchangeInfo.toCoinAmount);
-  const [coinList, setCoinList] = useState([]);
   const [tempCoinList, setTempCoinList] = useState([]);
   const [searchValue1, setSearchValue1] = useState('');
   const [searchValue2, setSearchValue2] = useState('');
@@ -27,9 +27,11 @@ const MainForm = (props) => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`https://api.simpleswap.io/get_all_currencies?api_key=${api_key}`);
-        setCoinList(response.data);
-        const tempList = response.data.map(item => ({ coin: item, visible: true }))
-        setTempCoinList(tempList)
+        const popularCoinList = popularCoins.map((item) => response.data.find((coin) => coin.symbol == item));
+        const allCoinList = response.data.filter((item) => !popularCoins.includes(item.symbol));
+        const coinList = [...popularCoinList, ...allCoinList];
+        const tempList = coinList.map(item => ({ coin: item, visible: true }));
+        setTempCoinList(tempList);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -119,7 +121,7 @@ const MainForm = (props) => {
             <div className='text-sm w-1/3 leading-tight'>{fromCoin.symbol.toUpperCase().slice(0, 4)}</div>
             <div className='w-1/3 flex justify-end'><IoIosArrowDown size={15} /></div>
           </button>
-          <div id="dropdown-menu" className={`${showDropdown1 ? '' : 'hidden'} absolute right-0 left-0 mx-4 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1`}>
+          <div id="dropdown-menu" className={`${showDropdown1 ? '' : 'hidden'} absolute right-0 left-0 mx-4 rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1`}>
             <form>
               <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
               <div className="relative">
@@ -133,7 +135,7 @@ const MainForm = (props) => {
                   id="search-input"
                   value={searchValue1}
                   onChange={handleSearch1}
-                  className="block w-full p-4 ps-10 text-[18px] text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
+                  className="block w-full p-4 ps-10 text-[18px] text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
                 <button type="button"
                   onClick={() => {
                     setShowDropdown1(!showDropdown1);
@@ -190,11 +192,11 @@ const MainForm = (props) => {
             <h2 className='text-[10px] ml-2 -mb-1  font-bold text-black/40'>You Get</h2>
             <div className='cursor-pointer'>
               {isLoading ? <div role="status">
-                  <svg aria-hidden="true" className="w-4 h-4 text-white animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                  </svg>
-                </div> : <input value={toCoinAmount}
+                <svg aria-hidden="true" className="w-4 h-4 text-white animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                </svg>
+              </div> : <input value={toCoinAmount}
                 className='focus:outline-none bg-gray-200 hover:bg-gray-200 text-black/60  w-full text-[18px] font-bold'
                 type="text" readOnly />}
             </div>
@@ -206,7 +208,7 @@ const MainForm = (props) => {
             <div className='text-sm w-1/3 leading-tight'>{toCoin.symbol.toUpperCase().slice(0, 4)}</div>
             <div className='w-1/3 flex justify-end'><IoIosArrowDown size={15} /></div>
           </button>
-          <div id="dropdown-menu" className={`${showDropdown2 ? '' : 'hidden'} absolute right-0 left-0 mx-4 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1`}>
+          <div id="dropdown-menu" className={`${showDropdown2 ? '' : 'hidden'} absolute right-0 left-0 mx-4 rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1`}>
             <form>
               <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
               <div className="relative">
@@ -220,7 +222,7 @@ const MainForm = (props) => {
                   value={searchValue2}
                   id="search-input"
                   onChange={handleSearch2}
-                  className="block w-full p-4 ps-10 text-[18px] text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" autoComplete="off" />
+                  className="block w-full p-4 ps-10 text-[18px] text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" autoComplete="off" />
                 <button type="button"
                   onClick={() => {
                     setShowDropdown2(!showDropdown2);
